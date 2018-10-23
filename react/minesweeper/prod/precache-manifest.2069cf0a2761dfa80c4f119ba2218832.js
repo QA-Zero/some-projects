09c4f30f453a99bee7dab474cdfdba6c2123fb34
unknown <solvegen.test@gmail.com>
2018-10-23 19:51:56 +0300

    1

--- react/minesweeper/prod/precache-manifest.2069cf0a2761dfa80c4f119ba2218832.js
+++ react/minesweeper/prod/precache-manifest.2069cf0a2761dfa80c4f119ba2218832.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "60e1f1b97d4d7ffd6bee",
-    "url": "/static/js/main.60e1f1b9.chunk.js"
+    "revision": "0aaf5efed606aaf90af0",
+    "url": "/static/js/main.0aaf5efe.chunk.js"
   },
   {
     "revision": "4d52c8c914fe3b4f731f",
     "url": "/static/js/1.4d52c8c9.chunk.js"
   },
   {
-    "revision": "60e1f1b97d4d7ffd6bee",
+    "revision": "0aaf5efed606aaf90af0",
     "url": "/static/css/main.d42d8d7a.chunk.css"
   },
   {
-    "revision": "28949ddb6b30aad28541932739747daf",
+    "revision": "eb1c404daf2c1110d2954133f5f42d27",
     "url": "/index.html"
   }
 ];
--- react/minesweeper/prod/service-worker.js
+++ react/minesweeper/prod/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7a20fa1dc2248aee3ce9abc8fb4984c7.js"
+  "/precache-manifest.2069cf0a2761dfa80c4f119ba2218832.js"
 );
 
 workbox.clientsClaim();
